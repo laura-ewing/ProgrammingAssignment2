271a6a139b4e152bf72dbd8407cb3f86701bb099
laura-ewing <laura.ewing@tutanota.com>
2021-11-23 12:19:40 -0600

    added code for makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## stores symmetrical matrix in cache. must first define matrix and use that
+## object as x
+## example
+## matrix1 <- matrix(1:4, 2, 2)
+## matrixcache <- makeCacheMatrix(matrix1)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## solves matrix defined above if not already done
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #returns matrix that is inverse of x
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
-
-#test
